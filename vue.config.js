d978448940f5e88853ee9079e7dfd1a437e674b3
DESKTOP-D7JMFLU\Max <trashbin4585@gmail.com>
2020-08-17 20:33:40 +0200

    fixed some thigns

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
-  productionSourceMap: false
+  productionSourceMap: false,
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/website-portfolio/" : "/"
 };
