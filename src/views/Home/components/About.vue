ff2775f5177fb455f80658fbe0ffafd88c63a0be
Max Netterberg <max.netterberg@gmail.com>
2022-05-05 19:42:52 +0200

    Removed unused store

--- src/views/Home/components/About.vue
+++ src/views/Home/components/About.vue
@@ -32,11 +32,9 @@
 </template>
 
 <script lang="ts" setup>
-import { useAppStore } from '@/store/app-store';
 import Button from 'primevue/button';
 import { ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
-const store = useAppStore();
 const { t } = useI18n();
 
 const image = ref<HTMLImageElement>();
